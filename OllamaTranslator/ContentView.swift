2808d9f3d49ec49366997e3419414fc3f56f66df
zimu <234047485@qq.com>
2025-05-18 18:22:30 +0800

    init

---
+++ OllamaTranslator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OllamaTranslator
+//
+//  Created by 荒野老男人 on 2025/5/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OllamaTranslator/OllamaTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  OllamaTranslatorApp.swift
+//  OllamaTranslator
+//
+//  Created by 荒野老男人 on 2025/5/18.
+//
+
+import SwiftUI
+
+@main
+struct OllamaTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
